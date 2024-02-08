a8129b22ad557d4e94e6425edb7b911b8a0d4713
Peter Sukhenko <7243796+pezzu@users.noreply.github.com>
2024-02-08 12:05:02 -0500

    Individual event files

---
+++ src/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.login(process.env.DISCORD_BOT_TOKEN);
