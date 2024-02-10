8b01155a718a10b3c58da815cded17cc22eda917
Peter Sukhenko <7243796+pezzu@users.noreply.github.com>
2024-02-10 18:37:47 -0500

    Reply with downloaded video

--- src/events/message.js
+++ src/events/message.js
@@ -1,13 +1,51 @@
 const fs = require("fs");
+const util = require("node:util");
 
-function processMessage(message) {
+const execFile = util.promisify(require("node:child_process").execFile);
+
+const DOWNLOAD_DIR = "/tmp";
+const SUPPORTED_URLS = ["https://twitter.com/", "https://x.com/"];
+
+async function processMessage(message) {
     console.log(`messagge received! ${message.content}`);
 
     if (message.author.bot) return;
 
-    if (message.content.startsWith("https://twitter.com/")) {
-        message.reply({ files: [fs.createReadStream(__dirname + "/../../1.mp4")] });
+    if (!isSupportedURL(extractURL(message.content))) return;
+
+    const files = await downloadMedia(message.content);
+
+    if (files.length > 0) {
+        await message.reply({ files });
     }
+
+    await cleanUp(files);
+}
+
+async function downloadMedia(url) {
+    const { stdout } = await execFile("yt-dlp", [
+        "--print",
+        "after_move:filepath",
+        "--path",
+        DOWNLOAD_DIR,
+        url,
+        "--restrict-filenames",
+        "--no-simulate",
+    ]);
+    const files = stdout.split("\n").slice(0, -1);
+    return files;
+}
+
+function extractURL(content) {
+    return content;
+}
+
+function isSupportedURL(url) {
+    return SUPPORTED_URLS.some((supportedURL) => url.startsWith(supportedURL));
+}
+
+function cleanUp(files) {
+    return Promise.all(files.map((file) => fs.promises.unlink(file)));
 }
 
 module.exports = { processMessage };
