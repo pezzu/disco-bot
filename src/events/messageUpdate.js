25e13773b7acdf6c7458ae5e56c86d9abd1cc53a
Peter Sukhenko <7243796+pezzu@users.noreply.github.com>
2024-02-15 18:42:45 -0500

    Fix ambiguous update message from Discord

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -4,7 +4,8 @@ const { replyWithMedia } = require("../media-fetch/message.js");
 module.exports = {
     name: Events.MessageUpdate,
     execute: (oldMessage, newMessage) => {
-        console.log(`Message updated: ${newMessage.content}`);
+        if (oldMessage.content === newMessage.content) return;
+        console.log(`Message updated: ${oldMessage.content} -> ${newMessage.content}`);
         replyWithMedia(newMessage).catch(console.error);
     },
 };
